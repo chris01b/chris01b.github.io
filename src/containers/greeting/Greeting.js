0ba6ac64529ea59a96b20754a804c15c3725a622
chris01b <cbid@sas.upenn.edu>
2024-06-13 17:31:50 -0400

    Put contact me button on same line as social icons, removed unused icon

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -36,18 +36,20 @@ export default function Greeting() {
               >
                 {greeting.subTitle}
               </p>
-              <SocialMedia />
-              <div className="button-greeting-div">
-                <Button text="Contact me" href="#contact" />
-                {greeting.resumeLink && (
-                  <a
-                    href={require("./resume.pdf")}
-                    download="Resume.pdf"
-                    className="download-link-button"
-                  >
-                    <Button text="Download my resume" />
-                  </a>
-                )}
+              <div className="social-greeting-div">
+                <SocialMedia />
+                <div className="button-greeting-div">
+                  <Button text="Contact me" href="#contact" />
+                  {greeting.resumeLink && (
+                    <a
+                      href={require("./resume.pdf")}
+                      download="Resume.pdf"
+                      className="download-link-button"
+                    >
+                      <Button text="Download my resume" />
+                    </a>
+                  )}
+                </div>
               </div>
             </div>
           </div>
